5e245243b12aa22041a327c37e71cbae4771f3ad
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-04-06 01:46:00 +0800

    done part 1 needs testing change the input stream of the file

--- EncryptionLab/DesSolution.java
+++ EncryptionLab/DesSolution.java
@@ -14,29 +14,47 @@ public class DesSolution {
         String line;
         BufferedReader bufferedReader = new BufferedReader( new FileReader(fileName));
         while((line= bufferedReader.readLine())!=null){
-            data = data +"\n" + line;
+            data = data + "\n" + line;
         }
         System.out.println("Original content: "+ data);
 
 //TODO: generate secret key using DES algorithm
-        
+        KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+        SecretKey desKey = keyGen.generateKey();
+
 //TODO: create cipher object, initialize the ciphers with the given key, choose encryption mode as DES
+        Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE,desKey);
 
 //TODO: do encryption, by calling method Cipher.doFinal().
+        byte[] textEncrypted = cipher.doFinal(data.getBytes());
 
 //TODO: print the length of output encrypted byte[], compare the length of file shorttext.txt and longtext.txt
 
+        //print content of files
+        System.out.println("The content of byte array "+ fileName + ": "+ textEncrypted.toString());
+        System.out.println("The byte array length of "+fileName+": "+textEncrypted.length);
+
 //TODO: do format conversion. Turn the encrypted byte[] format into base64format String using Base64
+        String base64format = Base64.getEncoder().encodeToString(textEncrypted);
 
 //TODO: print the encrypted message (in base64format String format)
+        System.out.println("The encrypted message in base64 format is :"+base64format);
 
 //TODO: create cipher object, initialize the ciphers with the given key, choose decryption mode as DES
 
+        //symmetric key right here so rmb to use the same key for encryption and decryption and not the different key!
+        Cipher decrypt = Cipher.getInstance("DES");
+        decrypt.init(Cipher.DECRYPT_MODE,desKey);
+
 //TODO: do decryption, by calling method Cipher.doFinal().
+        byte[] textDecrypted = cipher.doFinal(textEncrypted);
 
 //TODO: do format conversion. Convert the decrypted byte[] to String, using "String a = new String(byte_array);"
+        String decryptedText = new String(textDecrypted);
 
 //TODO: print the decrypted String text and compare it with original text
-
+        System.out.println("The decryted text is "+decryptedText);
+        System.out.println("The original text is "+data);
     }
 }
